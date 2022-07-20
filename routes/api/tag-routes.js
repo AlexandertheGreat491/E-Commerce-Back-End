ce9f4d95a543f6592d9f8d32372684b40ceaeb9c
agvan <agvandyke25@gmail.com>
2022-07-20 10:43:43 -0500

    updated all comments

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,29 +2,29 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-// find all tags
-// be sure to include its associated Product data
+// finds all tags
+//  includes its associated Product data
 router.get('/', (req, res) => {
   
 });
 
-// find a single tag by its `id`
-// be sure to include its associated Product data
+// finds a single tag by its `id`
+//  includes its associated Product data
 router.get('/:id', (req, res) => {
   
 });
 
-// create a new tag
+// creates a new tag
 router.post('/', (req, res) => {
   
 });
 
-// update a tag's name by its `id` value
+// updates a tag's name by its `id` value
 router.put('/:id', (req, res) => {
   
 });
 
-// delete on tag by its `id` value
+// deletes a tag by its `id` value
 router.delete('/:id', (req, res) => {
   
 });
